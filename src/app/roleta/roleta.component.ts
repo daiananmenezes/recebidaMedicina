0a2db52dbee5473a4502026f849c8e5af9098c58
daiana.menezes <daiana.menezes@mjv.com.br>
2024-02-28 17:07:00 -0300

    ajuste botão

--- src/app/roleta/roleta.component.ts
+++ src/app/roleta/roleta.component.ts
@@ -14,20 +14,26 @@ export class RoletaComponent {
     const wheel = document.getElementById("wheel") as HTMLCanvasElement;
     const spinBtn = document.getElementById("spin-btn") as HTMLButtonElement;
     const finalValue = document.getElementById("final-value") as HTMLElement;
+    const icon = document.getElementById("icon") as HTMLButtonElement;
 
-// Array de frases para a roda
-const phrases = [
-  "1 shotzinho",
-  "Fale o nome completo da Pauli e da Isis, ou tome um shot",
-  "Chame alguém para uma competição de dança, quem perder, bebe",
-  "Gire a roleta",
-  "Escolha alguém para beber um shot",
-  "Se deu mal, 2 shots",
-  "Fale o nome completo da Pauli e da Isis, ou tome um shot",
-  "Chame alguém para uma competição de dança, quem perder, bebe",
-  "Gire a roleta",
-  "Beba um shot e escolha alguém para beber junto",
-];
+    const phrases = [
+      "1 shotzinho",
+      "Fale o nome completo da Pauli e da Isis. Se não souber, tome um shot",
+      "Chame alguém para uma competição de dança. Quem perder, bebe",
+      "Gire a roleta",
+      "Escolha alguém para beber um shot",
+      "Se deu mal, 2 shots",
+      "Qual a idade da Isis e da Pauli? Se não souber, beba um shot",
+      "Fale o nome completo da Pauli e da Isis. Se não souber, tome um shot",
+      "Desafie alguém para uma competição de dança. Quem perder, bebe",
+      "Gire a roleta",
+      "Beba um shot e escolha alguém para beber junto",
+      "Conte uma piada engraçada. Se ninguém rir, tome um shot",
+      "Qual a idade da Isis e da Pauli? Se não souber, beba um shot",
+      "Desafie alguém para um duelo de polegar. Quem perder, bebe",
+      "Você se safou! Escolha duas pessoas para beber"
+    ];
+    
 
 // Função para selecionar uma frase aleatória
 const getRandomPhrase = () => {
@@ -39,6 +45,12 @@ const getRandomPhrase = () => {
 spinBtn.addEventListener("click", () => {
   spinBtn.disabled = true;
   finalValue.innerHTML = ""; // Limpar o conteúdo anterior
+  spinBtn.classList.add("disabled"); 
+  spinBtn.style.opacity = '0.1'; 
+  spinBtn.style.cursor = 'not-allowed'; 
+  spinBtn.style.background = 'rgb(56, 54, 54)'; 
+  spinBtn.style.color = 'rgb(97, 95, 95)';
+  icon.style.color  = 'rgb(97, 95, 95)';
 
   // Adicionar frase aleatória em um elemento de parágrafo para animação
   const p = document.createElement("p");
@@ -54,6 +66,11 @@ spinBtn.addEventListener("click", () => {
       // Selecionar uma frase aleatória e exibir
       finalValue.innerHTML = `<p>${getRandomPhrase()}</p>`;
       spinBtn.disabled = false;
+      spinBtn.style.opacity = '1'; // Restaurar a opacidade
+      spinBtn.style.cursor = 'pointer'; // Restaurar o cursor
+      spinBtn.style.background = 'white'; // Restaurar o fundo
+      spinBtn.style.color = 'rgb(173, 11, 11)'; // Restaurar a cor do texto
+      icon.style.color  = 'rgb(173, 11, 11)';
     }
   }, 200); // Tempo de exibição de cada frase (200 milissegundos)
 });
